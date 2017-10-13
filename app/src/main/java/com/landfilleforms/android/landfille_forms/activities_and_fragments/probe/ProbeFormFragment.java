3aa9854b0723339c69442fcfed0b0ae8a46e37ee
Alvin Quach <alvinquach91@gmail.com>
2017-10-13 01:37:35 -0700

    fixed some shit

--- app/src/main/java/com/landfilleforms/android/landfille_forms/activities_and_fragments/probe/ProbeFormFragment.java
+++ app/src/main/java/com/landfilleforms/android/landfille_forms/activities_and_fragments/probe/ProbeFormFragment.java
@@ -307,7 +307,6 @@ public class ProbeFormFragment extends Fragment {
             //END OF OG
             // START OF NEW
             if (mProbeData.getMethanePercentage() >= 5.0) {
-//<<<<<<< HEAD
                 mMethanePercentageView.setTextColor(Color.RED);
             }
             else if (mProbeData.getMethanePercentage() >= 0.1 && mProbeData.getMethanePercentage() < 4.9) {
@@ -328,30 +327,6 @@ public class ProbeFormFragment extends Fragment {
             }
             //END OF NEW
 
-//=======
-                //mProbeNumberView.setTextColor(Color.RED);
-                //mWaterPressureView.setTextColor(Color.RED);
-                mMethanePercentageView.setTextColor(Color.RED);
-            }
-            else if (mProbeData.getMethanePercentage() >= 0.1 && mProbeData.getMethanePercentage() < 4.9) {
-               // mProbeNumberView.setTextColor(Color.rgb(255,165,0));
-                mWaterPressureView.setTextColor(Color.rgb(255,165,0));
-                mMethanePercentageView.setTextColor(Color.rgb(255,165,0));
-            }
-            else {
-               // mProbeNumberView.setTextColor(Color.WHITE);
-                //mWaterPressureView.setTextColor(Color.WHITE);
-                mMethanePercentageView.setTextColor(Color.WHITE);
-            }
-            if(mProbeData.getWaterPressure() >1.0){
-                mWaterPressureView.setTextColor(Color.RED);
-            }
-            else if(mProbeData.getWaterPressure() >= 0.2 && mProbeData.getWaterPressure() <= 0.9){
-                mWaterPressureView.setTextColor(Color.rgb(255,165, 0));
-            }
-            else
-                mWaterPressureView.setTextColor(Color.WHITE);
->>>>>>> 083e3b555c682a1b2f033eb58d6af9d6640772c3
         }
 
         @Override
